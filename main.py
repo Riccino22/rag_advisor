1b8a5e676f34edfab8d294508ad99fde266913ce
riccino-ot <riccardorinojosam@gmail.com>
2025-04-23 12:28:45 -0300

    feat: Enhance Streamlit app with audio transcription and environment setup
    - Add dotenv support for environment variables
    - Integrate Groq for audio transcription functionality
    - Update .gitignore to include .vscode and .env
    - Add new dependencies in requirements.txt for Groq and dotenv

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 import streamlit as st
+import os
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
 
 st.title("Preguntame algo")
 question = st.audio_input("Pregunta algo")
+
+if question:
+    question_file = (question.name, question.getvalue())
+    print(2+2)
+
+    client = Groq()
+    # filename = os.path.dirname(__file__) + "/audio.m4a"
+
+    transcription = client.audio.transcriptions.create(
+      file=question_file,
+      model="whisper-large-v3",
+      response_format="verbose_json"
+    )
+    print(transcription.text)
